8e9e23cf4370550e0db1c2cd680efe5374af610b
joe lazarz <jlazarz87@gmail.com>
2019-09-23 10:21:07 -0400

    kit serializer

--- app/serializers/kit_serializer.rb
+++ app/serializers/kit_serializer.rb
@@ -1,15 +1,6 @@
 class KitSerializer < ActiveModel::Serializer
+  attributes :id, :name, :detail, :sample_path, :image
   
-  include Rails.application.routes.url_helpers
-
-  attributes :id, :name, :detail, :sample
-
-  def sample
-    rails_blob_url(object.sample) if object.sample.attached?
-  end
-  
-  # attributes :id, :name, :detail, :sample, :image
-
   has_many :notes
   has_many :genres
   has_many :digs
