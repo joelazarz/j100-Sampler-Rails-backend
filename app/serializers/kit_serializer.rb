a1b9de79cc13743fa5e2ddc4eb46090eeb9ad2fd
joe lazarz <jlazarz87@gmail.com>
2019-10-02 07:59:39 -0400

    commented out genre and crate models

--- app/serializers/kit_serializer.rb
+++ app/serializers/kit_serializer.rb
@@ -1,8 +1,8 @@
 class KitSerializer < ActiveModel::Serializer
   attributes :id, :name, :detail, :sample, :image
   
-  has_many :crates
+  # has_many :crates
   has_many :notes
-  has_many :genres
+  # has_many :genres
   has_many :digs
 end
